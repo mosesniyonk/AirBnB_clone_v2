5b731202ce3f668e63b7a91f10ca4c48ab6ea31e
mosesniyonk <mosesniyonk@outlook.com>
2024-06-24 11:34:15 +0200

    refactor: Update Amenity model to include missing column and relationship

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
+""" State Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+
 
 class Amenity(BaseModel):
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity")
-    
+    name = ""
