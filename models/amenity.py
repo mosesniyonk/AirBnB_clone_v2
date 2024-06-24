1a0158ccfa1409a4b05f07b88f99f311c23aee0b
mosesniyonk <mosesniyonk@outlook.com>
2024-06-24 11:32:19 +0200

    refactor: Add missing column and relationship in Amenity model

--- models/amenity.py
+++ models/amenity.py
@@ -8,3 +8,4 @@ class Amenity(BaseModel):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity")
+    
